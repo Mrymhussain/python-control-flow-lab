513517b757af382fbe3de9881e3b2fd3f77822b6
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-20 14:27:15 +0300

    Ex0

---
+++ exercises.py
@@ -0,0 +1,8 @@
+# Exercise 0: Example
+def print_greeting():
+    # Your code goes here. Remember to indent!
+    python_is_fun = True
+    if python_is_fun:
+        print("Python is fun!")
+print_greeting()
+
