d5d75d994c06517c6749555386c37d40fff3ce26
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-20 14:34:05 +0300

    Ex1 is done

--- exercises.py
+++ exercises.py
@@ -6,3 +6,15 @@ def print_greeting():
         print("Python is fun!")
 print_greeting()
 
+# Exercise 1: Vowel or Consonant
+def check_letter():
+    letter = input("Enter a letter (a-z or A-Z): ")
+
+    if len(letter) != 1 or not letter.isalpha():
+        print("Invalid input.")
+    elif letter.lower() in "aeiou":
+        print(f"The letter {letter} is a vowel.")
+    else:
+        print(f"The letter {letter} is a consonant.")
+
+check_letter()
