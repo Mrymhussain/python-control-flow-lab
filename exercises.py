3dfe2ec97b42c4a0d6616514cf440e1f9d23186a
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-20 14:40:20 +0300

    Ex2 is done

--- exercises.py
+++ exercises.py
@@ -18,3 +18,22 @@ def check_letter():
         print(f"The letter {letter} is a consonant.")
 
 check_letter()
+
+
+# Exercise 2: Old enough to vote?
+def check_voting_eligibility():
+    try:
+        age = int(input("Please enter your age: "))
+        voting_age = 18
+
+        if age < 0:
+            print("Invalid age.")
+        elif age >= voting_age:
+            print("You are eligible to vote.")
+        else:
+            print("You are not eligible to vote.")
+
+    except ValueError:
+        print("Please enter a valid age.")
+
+check_voting_eligibility()
